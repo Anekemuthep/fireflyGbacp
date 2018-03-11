019981006cf76c1024e9b29734e0b31d208f8c2b
Alfonso Bustamante <athal.zigma@gmail.com>
2018-03-11 16:23:24 -0300

    Se hicieron cambios

--- src/Parameters/Gbacp.hs
+++ src/Parameters/Gbacp.hs
@@ -1,5 +1,14 @@
-module Parameters.Gbacp where
+module Parameters.Gbacp (function, numberSet) where
+import System.IO
+import Lib.Prelude  
 
-main = do line <- getLine
-  let lineRev = reverse line
-  putStrLn $ "you said " ++ lineRev ++ " backwards!"
+function :: Int -> Int
+function x = 2*x
+
+numberSet :: [Int]
+numberSet = [1,2,3,4,5,6]
+
+
+--main :: IO ()
+--main = do line <- getLine
+--  putStrLn $ "you said " ++ line ++ " backwards!"
